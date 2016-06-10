5a98339f2b2a3b3eb539729b374e4cc6c2bf28d3
Jacob Greenberg <jgreenberg45@yahoo.com>
2016-06-10 19:40:48 -0400

    Basic HTML struct

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var server = express();
+
+
+server.get('/', function(request, response){
+  response.sendFile('public/html/index.html', {root: __dirname});
+});
+
+server.get('/about', function(request, response){
+  response.sendFile('public/html/info.html', {root: __dirname});
+});
+
+server.get('/contact', function(request, response){
+    response.sendFile('public/html/Contact.html', {root: __dirname});
+});
+
+server.listen(8080, function(){
+  console.log('now listening on port 8080');
+});
